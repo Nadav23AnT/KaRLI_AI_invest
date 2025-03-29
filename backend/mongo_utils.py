612f251a9a0721b1302952dd5ad0247a92030d41
itai124 <itai.shulhami@gmail.com>
2025-03-29 16:25:55 +0300

    feat: add brokerApi's cerds to the mongo signUp function-(add to user document)

--- backend/mongo_utils.py
+++ backend/mongo_utils.py
@@ -9,12 +9,19 @@ db = client["KaRLi"]
 users_collection = db["users"]
 
 
-def sign_up(username, password, age, risk):
+def sign_up(username, password, age, risk, broker_api_key, broker_api_secret):
     document = users_collection.find_one({"username": username})
 
     if document:
         return False
-    users_collection.insert_one({"username": username, "password": password, "age": age, "risk_level": risk})
+    users_collection.insert_one({
+        "username": username,
+        "password": password,
+        "age": age,
+        "risk_level": risk,
+        "brokerApiKey": broker_api_key,
+        "brokerApiSecret": broker_api_secret
+    })
     return True
 
 
