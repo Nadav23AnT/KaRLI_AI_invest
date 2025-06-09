5fa9f190bd2b7a0d6fd41bdff8b0c41cc190db41
itai124 <itai.shulhami@gmail.com>
2025-06-09 16:49:30 +0300

    feat: fetch all users function

--- backend/mongo_utils.py
+++ backend/mongo_utils.py
@@ -42,3 +42,21 @@ def get_user_brokerApi_credentials(username):
             "api_secret": document.get("brokerApiSecret")
         }
     return None
+
+def get_all_users_with_credentials():
+    users = []
+
+    for user in users_collection.find({}):
+        username = user.get("username")
+        risk = user.get("risk_level")
+        api_key = user.get("brokerApiKey")
+        api_secret = user.get("brokerApiSecret")
+
+        users.append({
+            "username": username,
+            "risk": risk,
+            "api_key": api_key,
+            "api_secret": api_secret,
+        })
+
+    return users
