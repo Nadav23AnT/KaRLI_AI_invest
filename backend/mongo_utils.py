8b6ca13057936e1f64fa12f639f5d42cf3bb11bc
itai124 <itai.shulhami@gmail.com>
2025-06-20 16:57:13 +0300

    feat: fetch_data_for_inference function and set global env variables

--- backend/mongo_utils.py
+++ backend/mongo_utils.py
@@ -1,5 +1,6 @@
 import os
 from pymongo import MongoClient
+from Enums import rl_variables
 
 MONGO_URI = os.getenv("MONGO_URI",
                       "mongodb+srv://adm:Aa123456@karli.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000")
@@ -10,8 +11,6 @@ stocksDB = client["stock_data_db"]
 norm_collection = stocksDB["daily_stock_data_normalized"]
 meta_collection = stocksDB["normalization_metadata"]
 
-FEATURE_COLS = ["rsi","tsi","stochastic","roc","cci","trix",
-                "mfi","cmf","bb_percent","adx","vi_pos"]
 
 def sign_up(username, password, age, risk, broker_api_key, broker_api_secret):
     document = users_collection.find_one({"username": username})
@@ -65,10 +64,10 @@ def get_all_users_with_credentials():
 
     return users
 
-def load_stats(ticker:str) -> dict:
+def load_stats(ticker: str) -> dict:
     stats = {d["feature"]: (d["mean"], d["std"])
              for d in meta_collection.find({"ticker": ticker})}
-    missing = set(FEATURE_COLS) - set(stats)
+    missing = set(rl_variables.FEATURE_COLS) - set(stats)
     if missing:
         raise ValueError(f"metadata missing for {missing}")
     return stats
@@ -77,5 +76,7 @@ def insert_daily_data(tickers_data):
     records = tickers_data.to_dict(orient='records')
 
     if records:
-        print(records)
-        # norm_collection.insert_many(records)
+        norm_collection.insert_many(records)
+
+def fetch_data_for_inference(ticker: str, window_size: int = 30):
+    return norm_collection.find({"ticker": ticker}).sort("date", -1).limit(window_size-1)
